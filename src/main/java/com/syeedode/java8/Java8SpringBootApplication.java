d0e8779f40f3e926f103217756ba6c179ae268c2
Syeed Ode <sy_ode@yahoo.com>
2018-04-03 13:37:06 -0400

    Updated java8 streams

---
+++ src/main/java/com/syeedode/java8/Java8SpringBootApplication.java
@@ -0,0 +1,16 @@
+package com.syeedode.java8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: syeedode
+ * Date: 2/23/18
+ */
+@SpringBootApplication
+public class Java8SpringBootApplication {
+
+    public static void main(String args []) {
+        SpringApplication.run(Java8SpringBootApplication.class, args);
+    }
+}
---
+++ src/main/java/com/syeedode/java8/imperativetostreams/ImperativeToStreamsController.java
@@ -0,0 +1,18 @@
+package com.syeedode.java8.imperativetostreams;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author: syeedode
+ * Date: 2/23/18
+ */
+@RestController
+public class ImperativeToStreamsController {
+
+    @RequestMapping(value = "/example", method = RequestMethod.GET)
+    public void anyExampleWillDo() {
+
+    }
+}
