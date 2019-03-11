82f1436c9fd1f6a38527fdd8e45089357abe88d5
Syeed Ode <syeedode@Syeeds-MacBook-Pro.local>
2019-03-11 14:21:07 -0400

    Minor mods to InnerClassDemo and RomanNumerTest

--- src/main/java/com/syeedode/hardcorejava/a_06_1_inner_classes/InnerClassDemo.java
+++ src/main/java/com/syeedode/hardcorejava/a_06_1_inner_classes/InnerClassDemo.java
@@ -1,12 +1,47 @@
 package com.syeedode.hardcorejava.a_06_1_inner_classes;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class InnerClassDemo extends JDialog {
 
     private int beepCount = 1;
+    private final Container contentPane;
+    private final String LOGO = "This is my logo";
 
     public InnerClassDemo(final int beepCount) {
+        super();
+        setTitle("Anonymous Demo");
+        contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
 
+        JLabel logoLabel = new JLabel(LOGO);
+        contentPane.add(BorderLayout.NORTH, logoLabel);
+        JButton btn = new BeepButton("Beep");
+        contentPane.add(BorderLayout.SOUTH, btn);
+        pack();
+        this.beepCount = beepCount;
+    }
+
+    private class BeepButton extends JButton implements ActionListener {
+
+        public BeepButton(final String text) {
+            super(text);
+            addActionListener(this);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                for(int count = 0; count < beepCount; count++) {
+                    Toolkit.getDefaultToolkit().beep();
+                    Thread.sleep(100);
+                }
+            } catch (final InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
     }
 }
--- src/test/java/com/syeedode/DemoRomanNumberalConversionTests.java
+++ src/test/java/com/syeedode/DemoRomanNumberalConversionTests.java
@@ -1,19 +1,17 @@
 package com.syeedode;
 
-import com.syeedode.wipro.DemoRomanNumeralConversion;
+import com.syeedode.interviews.wipro.DemoRomanNumeralConversion;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-public class AffinityMainClassTests {
+public class DemoRomanNumberalConversionTests {
 
 	@Test
 	public void contextLoads() {
 		Assert.assertEquals(11, DemoRomanNumeralConversion.getDecimalResultValue("XI"));
 		Assert.assertEquals(9, DemoRomanNumeralConversion.getDecimalResultValue("IX"));
 		Assert.assertEquals(21, DemoRomanNumeralConversion.getDecimalResultValue("XXI"));
+		Assert.assertEquals(19, DemoRomanNumeralConversion.getDecimalResultValue("XIX"));
 		Assert.assertEquals(19, DemoRomanNumeralConversion.getDecimalResultValue("XIX"));
 	}
 
